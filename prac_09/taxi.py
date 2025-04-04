bbf8d6aaeac92de3be204c963fa546ac957cb2ed
xie0801 <2729234855@qq.com>
2025-04-04 15:37:21 +0800

    Add files via upload

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -8,10 +8,11 @@ from prac_09.car import Car
 class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
 
-    def __init__(self, name, fuel, price_per_km):
+    price_per_km = 1.23  # Class variable for price per kilometer
+
+    def __init__(self, name, fuel):
         """Initialise a Taxi instance, based on parent class Car."""
         super().__init__(name, fuel)
-        self.price_per_km = price_per_km
         self.current_fare_distance = 0
 
     def __str__(self):
@@ -20,7 +21,7 @@ class Taxi(Car):
 
     def get_fare(self):
         """Return the price for the taxi trip."""
-        return self.price_per_km * self.current_fare_distance
+        return round(self.price_per_km * self.current_fare_distance, 1)
 
     def start_fare(self):
         """Begin a new fare."""
